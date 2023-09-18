727b8a6a66c15928823da624da33e1012c773a3e
MateuszRoman <6198355+gruberrro@users.noreply.github.com>
2023-09-18 13:19:09 +0200

    fix(requirements): bump requests and client encoding

--- onesky/client.py
+++ onesky/client.py
@@ -3,16 +3,20 @@ import os
 import requests
 import time
 
-DEFAULT_API_URL = 'https://platform.api.onesky.io/1/'
+DEFAULT_API_URL = "https://platform.api.onesky.io/1/"
 
 
 # python wrapper for OneSky's REST API, see
 # https://github.com/onesky/api-documentation-platform
 class Client:
-    def __init__(self, api_key, api_secret,
-                 api_url=DEFAULT_API_URL,
-                 download_dir='.',
-                 request_callback=None):
+    def __init__(
+        self,
+        api_key,
+        api_secret,
+        api_url=DEFAULT_API_URL,
+        download_dir=".",
+        request_callback=None,
+    ):
         self.api_url = api_url
         self.api_key = api_key
         self.api_secret = api_secret
@@ -23,17 +27,18 @@ class Client:
         timestamp = str(int(time.time()))
 
         dev_hash = hashlib.md5()
-        dev_hash.update(timestamp)
-        dev_hash.update(self.api_secret)
+        dev_hash.update(timestamp.encode("utf-8"))
+        dev_hash.update(self.api_secret.encode("utf-8"))
 
         return {
-            'api_key': self.api_key,
-            'timestamp': timestamp,
-            'dev_hash': dev_hash.hexdigest()
+            "api_key": self.api_key,
+            "timestamp": timestamp,
+            "dev_hash": dev_hash.hexdigest(),
         }
 
-    def do_http_request(self, relative_url, parameters=None, method='GET',
-                        upload_file_stream=None):
+    def do_http_request(
+        self, relative_url, parameters=None, method="GET", upload_file_stream=None
+    ):
         absolute_url = self.api_url + relative_url
 
         # the auth variables and any additional parameters are merged and
@@ -44,12 +49,13 @@ class Client:
             # by rule, filter out None.  this makes things a bit easier for the
             # caller because he doesn't have to do the filtering for default
             # variables.
-            url_parameters = dict([(k, v) for k, v in parameters.items()
-                                   if v is not None])
+            url_parameters = dict(
+                [(k, v) for k, v in parameters.items() if v is not None]
+            )
         url_parameters.update(self.create_auth_variables())
 
         if upload_file_stream is not None:
-            files = {'file': upload_file_stream}
+            files = {"file": upload_file_stream}
         else:
             files = None
 
@@ -59,28 +65,23 @@ class Client:
         # method is something like GET or POST or DELETE, for which we grab the
         # appropriate function from the requests library.
         request_function = getattr(requests, method.lower())
-        response = request_function(absolute_url,
-                                    params=url_parameters,
-                                    files=files)
+        response = request_function(absolute_url, params=url_parameters, files=files)
 
-        if (response.headers.get('content-disposition', '').
-                startswith('attachment;')):
+        if response.headers.get("content-disposition", "").startswith("attachment;"):
             # the response body is the contents of a file.  We save to a file
             # here and return 'filename' in the response dictionary.
 
             # the filename is in the 'content-disposition' header, in the form
             # "attachment; filename=hi-IN.po".  simplest to just split on = to
             # find it.
-            short_filename = (
-                response.headers['content-disposition'].split('=')[1]
-            )
+            short_filename = response.headers["content-disposition"].split("=")[1]
 
             absolute_filename = os.path.join(self.download_dir, short_filename)
-            with open(absolute_filename, 'wb') as f:
+            with open(absolute_filename, "wb") as f:
                 for chunk in response.iter_content():
                     f.write(chunk)
 
-            response_dict = {'downloaded_filename': absolute_filename}
+            response_dict = {"downloaded_filename": absolute_filename}
         else:
             # a json response is requested.  some requests (such as
             # project_group_delete) don't return anything, so we'll just return
@@ -95,88 +96,105 @@ class Client:
     ################################################################
     # project group API
     def project_group_list(self, page=None, per_page=None):
-        relative_url = 'project-groups'
-        params = {'page': page, 'per_page': per_page}
+        relative_url = "project-groups"
+        params = {"page": page, "per_page": per_page}
         return self.do_http_request(relative_url, params)
 
     def project_group_show(self, project_group_id):
-        relative_url = 'project-groups/{}'.format(project_group_id)
+        relative_url = "project-groups/{}".format(project_group_id)
         return self.do_http_request(relative_url)
 
     def project_group_create(self, name, locale=None):
-        relative_url = 'project-groups'
-        params = {'name': name, 'locale': locale}
-        return self.do_http_request(relative_url, params, method='POST')
+        relative_url = "project-groups"
+        params = {"name": name, "locale": locale}
+        return self.do_http_request(relative_url, params, method="POST")
 
     def project_group_delete(self, project_group_id):
-        relative_url = 'project-groups/{}'.format(project_group_id)
-        return self.do_http_request(relative_url, method='DELETE')
+        relative_url = "project-groups/{}".format(project_group_id)
+        return self.do_http_request(relative_url, method="DELETE")
 
     def project_group_languages(self, project_group_id):
-        relative_url = 'project-groups/{}/languages'.format(project_group_id)
+        relative_url = "project-groups/{}/languages".format(project_group_id)
         return self.do_http_request(relative_url)
+
     ################################################################
 
     ################################################################
     # project API
     def project_list(self, project_group_id, page=None, per_page=None):
-        relative_url = 'project-groups/{}/projects'.format(project_group_id)
-        params = {'page': page, 'per_page': per_page}
+        relative_url = "project-groups/{}/projects".format(project_group_id)
+        params = {"page": page, "per_page": per_page}
         return self.do_http_request(relative_url, params)
 
     def project_show(self, project_id):
-        relative_url = 'projects/{}'.format(project_id)
+        relative_url = "projects/{}".format(project_id)
         return self.do_http_request(relative_url)
 
-    def project_create(self, project_group_id, project_type,
-                       name=None, description=None):
-        relative_url = 'project-groups/{}/projects'.format(project_group_id)
-        params = {'project_type': project_type,
-                  'name': name, 'description': description}
-        return self.do_http_request(relative_url, params, method='POST')
+    def project_create(
+        self, project_group_id, project_type, name=None, description=None
+    ):
+        relative_url = "project-groups/{}/projects".format(project_group_id)
+        params = {
+            "project_type": project_type,
+            "name": name,
+            "description": description,
+        }
+        return self.do_http_request(relative_url, params, method="POST")
 
     def project_update(self, project_id, name=None, description=None):
-        relative_url = 'projects/{}'.format(project_id)
-        params = {'name': name, 'description': description}
-        return self.do_http_request(relative_url, params, method='PUT')
+        relative_url = "projects/{}".format(project_id)
+        params = {"name": name, "description": description}
+        return self.do_http_request(relative_url, params, method="PUT")
 
     def project_delete(self, project_id):
-        relative_url = 'projects/{}'.format(project_id)
-        return self.do_http_request(relative_url, method='DELETE')
+        relative_url = "projects/{}".format(project_id)
+        return self.do_http_request(relative_url, method="DELETE")
 
     def project_languages(self, project_id):
-        relative_url = 'projects/{}/languages'.format(project_id)
+        relative_url = "projects/{}/languages".format(project_id)
         return self.do_http_request(relative_url)
+
     ################################################################
 
     # project type
     def project_type_list(self):
-        return self.do_http_request('project-types')
+        return self.do_http_request("project-types")
 
     ################################################################
     # file API
     def file_list(self, project_id, page=None, per_page=None):
-        relative_url = 'projects/{}/files'.format(project_id)
-        params = {'page': page, 'per_page': per_page}
+        relative_url = "projects/{}/files".format(project_id)
+        params = {"page": page, "per_page": per_page}
         return self.do_http_request(relative_url, params)
 
     # file_name must be a file that can be read with open().  Allowed
     # file_formats are listed in the documentation; I'm using GNU_POT a lot for
     # testing.
-    def file_upload(self, project_id, file_name, file_format,
-                    locale=None, is_keeping_all_strings=None):
-        relative_url = 'projects/{}/files'.format(project_id)
-        params = {'file_format': file_format, 'locale': locale,
-                  'is_keeping_all_strings': is_keeping_all_strings}
+    def file_upload(
+        self,
+        project_id,
+        file_name,
+        file_format,
+        locale=None,
+        is_keeping_all_strings=None,
+    ):
+        relative_url = "projects/{}/files".format(project_id)
+        params = {
+            "file_format": file_format,
+            "locale": locale,
+            "is_keeping_all_strings": is_keeping_all_strings,
+        }
 
-        with open(file_name, 'rb') as file_stream:
-            return self.do_http_request(relative_url, params, method='POST',
-                                        upload_file_stream=file_stream)
+        with open(file_name, "rb") as file_stream:
+            return self.do_http_request(
+                relative_url, params, method="POST", upload_file_stream=file_stream
+            )
 
     def file_delete(self, project_id, file_name):
-        relative_url = 'projects/{}/files'.format(project_id)
-        params = {'file_name': file_name}
-        return self.do_http_request(relative_url, params, method='DELETE')
+        relative_url = "projects/{}/files".format(project_id)
+        params = {"file_name": file_name}
+        return self.do_http_request(relative_url, params, method="DELETE")
+
     ################################################################
 
     ################################################################
@@ -184,32 +202,35 @@ class Client:
 
     # TODO: this doesn't actually work; we need to get the file out of the
     # payload that's returned.
-    def translation_export(self, project_id, locale,
-                           source_file_name, export_file_name=None):
-        relative_url = 'projects/{}/translations'.format(project_id)
-        params = {'locale': locale, 'source_file_name': source_file_name,
-                  'export_file_name': export_file_name}
+    def translation_export(
+        self, project_id, locale, source_file_name, export_file_name=None
+    ):
+        relative_url = "projects/{}/translations".format(project_id)
+        params = {
+            "locale": locale,
+            "source_file_name": source_file_name,
+            "export_file_name": export_file_name,
+        }
         return self.do_http_request(relative_url, params)
 
     def translation_status(self, project_id, file_name, locale):
-        relative_url = 'projects/{}/translations/status'.format(project_id)
-        params = {'file_name': file_name, 'locale': locale}
+        relative_url = "projects/{}/translations/status".format(project_id)
+        params = {"file_name": file_name, "locale": locale}
         return self.do_http_request(relative_url, params)
+
     ################################################################
 
     ################################################################
     # import task
-    def import_task_list(self, project_id, page=None, per_page=None,
-                         status=None):
-        relative_url = 'projects/{}/import-tasks'.format(project_id)
-        params = {'page': page, 'per_page': per_page, 'status': status}
+    def import_task_list(self, project_id, page=None, per_page=None, status=None):
+        relative_url = "projects/{}/import-tasks".format(project_id)
+        params = {"page": page, "per_page": per_page, "status": status}
         return self.do_http_request(relative_url, params)
 
     def import_task_show(self, project_id, import_id):
-        relative_url = 'projects/{}/import-tasks/{}'.format(
-            project_id, import_id
-        )
+        relative_url = "projects/{}/import-tasks/{}".format(project_id, import_id)
         return self.do_http_request(relative_url)
+
     ################################################################
 
     # screenshot
@@ -218,63 +239,74 @@ class Client:
         pass
 
     # quotation
-    def quotation_show(self, project_id,
-                       files, to_locale,
-                       is_including_not_translated=None,
-                       is_including_not_approved=None,
-                       is_including_outdated=None,
-                       specialization=None):
-        relative_url = 'projects/{}/quotations'.format(project_id)
-        params = {'files': str(files),
-                  'to_locale': to_locale,
-                  'is_including_not_translated': is_including_not_translated,
-                  'is_including_not_approved': is_including_not_approved,
-                  'is_including_outdated': is_including_outdated,
-                  'specialization': specialization}
+    def quotation_show(
+        self,
+        project_id,
+        files,
+        to_locale,
+        is_including_not_translated=None,
+        is_including_not_approved=None,
+        is_including_outdated=None,
+        specialization=None,
+    ):
+        relative_url = "projects/{}/quotations".format(project_id)
+        params = {
+            "files": str(files),
+            "to_locale": to_locale,
+            "is_including_not_translated": is_including_not_translated,
+            "is_including_not_approved": is_including_not_approved,
+            "is_including_outdated": is_including_outdated,
+            "specialization": specialization,
+        }
         return self.do_http_request(relative_url, params)
 
     ################################################################
     # order
     def order_list(self, project_id, page=None, per_page=None):
-        relative_url = 'projects/{}/orders'.format(project_id)
-        params = {'page': page, 'per_page': per_page}
+        relative_url = "projects/{}/orders".format(project_id)
+        params = {"page": page, "per_page": per_page}
         return self.do_http_request(relative_url, params)
 
     def order_show(self, project_id, order_id):
-        relative_url = 'projects/{}/orders/{}'.format(
-            project_id, order_id
-        )
+        relative_url = "projects/{}/orders/{}".format(project_id, order_id)
         return self.do_http_request(relative_url)
 
-    def order_create(self, project_id,
-                     files, to_locale,
-                     order_type=None,
-                     is_including_not_translated=None,
-                     is_including_not_approved=None,
-                     is_including_outdated=None,
-                     translator_type=None,
-                     tone=None,
-                     specialization=None,
-                     note=None):
-        relative_url = 'projects/{}/orders'.format(project_id)
+    def order_create(
+        self,
+        project_id,
+        files,
+        to_locale,
+        order_type=None,
+        is_including_not_translated=None,
+        is_including_not_approved=None,
+        is_including_outdated=None,
+        translator_type=None,
+        tone=None,
+        specialization=None,
+        note=None,
+    ):
+        relative_url = "projects/{}/orders".format(project_id)
 
         # NOTE: it looks like 'files' should be a comma-separate string,
         # despite the documentation giving the example ['string.po'].  In other
         # words, it should just be "string.po" or "string1.po,string2.po".  I
         # know the former case works, anyway -- haven't yet tested the latter.
-        params = {'files': files,
-                  'to_locale': to_locale,
-                  'order_type': order_type,
-                  'is_including_not_translated': is_including_not_translated,
-                  'is_including_not_approved': is_including_not_approved,
-                  'is_including_outdated': is_including_outdated,
-                  'translator_type': translator_type,
-                  'tone': tone,
-                  'specialization': specialization,
-                  'note': note}
-        return self.do_http_request(relative_url, params, 'POST')
+        params = {
+            "files": files,
+            "to_locale": to_locale,
+            "order_type": order_type,
+            "is_including_not_translated": is_including_not_translated,
+            "is_including_not_approved": is_including_not_approved,
+            "is_including_outdated": is_including_outdated,
+            "translator_type": translator_type,
+            "tone": tone,
+            "specialization": specialization,
+            "note": note,
+        }
+        return self.do_http_request(relative_url, params, "POST")
+
     ################################################################
 
     # locale
     def locale_list(self):
-        return self.do_http_request('locales')
+        return self.do_http_request("locales")
--- setup.py
+++ setup.py
@@ -3,18 +3,15 @@
 from setuptools import setup
 
 setup(
-    name='onesky-python',
-    version='1.0.2',
-    description='python wrapper for the OneSky REST API',
-    author='Jana Mobile',
-    author_email='technology@jana.com',
-    maintainer='Dan OBrien',
-    maintainer_email='danob@jana.com',
-    install_requires=[
-        'requests==2.3.0',
-        'termcolor==1.1.0'
-    ],
-    license='LICENSE.txt',
-    packages=['onesky'],
-    url='https://github.com/Jana-Mobile/onesky-python'
+    name="onesky-python",
+    version="1.0.2",
+    description="python wrapper for the OneSky REST API",
+    author="Jana Mobile",
+    author_email="technology@jana.com",
+    maintainer="Dan OBrien",
+    maintainer_email="danob@jana.com",
+    install_requires=["requests==2.31.0", "termcolor==1.1.0"],
+    license="LICENSE.txt",
+    packages=["onesky"],
+    url="https://github.com/Human-It-Group/onesky-python",
 )
